d7ea1db53728b11e59f41b754d98f99b7b1f8dd9
Manju KC <kcmanjuthapa@gmail.com>
2019-10-17 16:03:58 -0400

    added handchange and handlesubmit functions

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,45 +1,76 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 const UpdateMovie = () => {
-    const initialItem = {
-        id:'',
-        title:'',
-        director:'',
-        metascore:'',
-        stars:''
-    }
-   const [stars, setStars] = useState([]);
-   const[update, setUpdate] = useState(initialItem )
+  const initialItem = {
+    id: props.match.params.id,
+    title: "",
+    director: "",
+    metascore: "",
+    stars: ""
+  };
+
+  const [stars, setStars] = useState([]);
+  const [update, setUpdate] = useState(initialItem);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUpdate({...update, [e.target.name]: e.target.value })
+
+  }
+  const changeHandlerStars = e => {
+    e.preventDefault();
+    setStars({...stars, [e.target.name]: [e.target.value] })
+}
+const data={
+    ...update,
+    ...stars
+}
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+    .put(`http://localhost:5000/api/movies/${props.match.params.id}`, data )
+    .catch(err => {
+        console.log(err.response)
+    })
+    props.history.push('/')
+    window.location.href = window.location.href
+  }
+
 
   return (
-  <div>
-      <form>
-          <input 
-          type='text'
-          name='title'
-          unchanged={handelChange}
-          value={'update.title'}
-          />
-          <input
-           type='text'
-           name='director'
-           unchanged={handelChange}
-           value='update.directtor'/>
-          <input
+    <div>
+      <form submit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          unchanged={handleChange}
+          value={update.title}
+        />
+        <input
+          type="text"
+          name="director"
+          unchanged={handleChange}
+          value={update.directtor}
+        />
+        <input
           type="number"
           name="metascore"
-          onChange={changeHandler}
-          value={update.metascore}/>
+          onChange={handleChange}
+          value={update.metascore}
+        />
 
-          <input
-           type="number"
-           name="metascore"
-           onChange={changeHandler}
-           value={update.stars}/>
-        
+        <input
+          type="array"
+          name="stars"
+          onChange={changeHandlerStars}
+          value={update.stars}
+        />
+        <button>Update Movie</button>
       </form>
-
-  </div>)
+    </div>
+  );
 };
 
 export default UpdateMovie;
