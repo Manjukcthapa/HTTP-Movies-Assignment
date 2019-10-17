a741267f6bcd11893fa29a4ccef8c91ee5fc7ffa
Manju KC <kcmanjuthapa@gmail.com>
2019-10-17 16:06:42 -0400

    added movie button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+import { NavLink , Route} from 'react-router-dom'
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -42,6 +43,11 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <NavLink to={`/update-movie/${this.state.movie.id}`}>
+        <button className="update-button">
+          Update
+        </button>
+        </NavLink>
       </div>
     );
   }
